c9d3193a27fdc4dfcf683b981e1b0f6ee274d4f4
vias <121429631+viasouo@users.noreply.github.com>
2024-06-17 04:48:32 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -245,5 +245,25 @@ st.download_button(
     data=csv,
     file_name='全國性公民投票概況(全國).csv',
     mime='csv'
-)'''
+)   
+
+#GitHub，連結按鈕
+st.link_button("前往GitHub", "https://github.com/viasouo/testwelcome",type="primary")
+
+
+#左邊選單
+st.markdown('')
+st.sidebar.link_button("前往ppt連結", "https://www.canva.com/design/DAGDZjMHv_8/PVkUtxYnHZOYqmFPptzadA/edit?utm_content=DAGDZjMHv_8&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",type="primary")
+st.sidebar.link_button("前往GitHub", "https://github.com/viasouo/testwelcome",type="primary")
+
+'''
 st.code(code, line_numbers=True)    
+
+#GitHub，連結按鈕
+st.link_button("前往GitHub", "https://github.com/viasouo/testwelcome",type="primary")
+
+
+#左邊選單
+st.markdown('')
+st.sidebar.link_button("前往canva-ppt連結", "https://www.canva.com/design/DAGDZjMHv_8/PVkUtxYnHZOYqmFPptzadA/edit?utm_content=DAGDZjMHv_8&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",type="primary")
+st.sidebar.link_button("前往GitHub", "https://github.com/viasouo/testwelcome",type="primary")
